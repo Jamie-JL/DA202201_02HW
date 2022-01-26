e6d46d41d299679666ea1352ce7bd16e6ea31acd
unknown <jh.jj@hotmail.com>
2022-01-25 22:44:41 -0800

    Takes two integer numbers and print calculations.

---
+++ input_output_2.py
@@ -0,0 +1,8 @@
+num1 = input("Type any integer number: ")
+
+num2 = input("Type another integer number: ")
+
+print(int(num1) + int(num2))
+print(int(num1) - int(num2))
+print(int(num1) * int(num2))
+print(float(int(num1) / int(num2)))
